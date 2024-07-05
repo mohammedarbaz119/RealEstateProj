047d4e5acd4bb90bab5243f22722d33571cf7f29
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2024-07-06 03:01:12 +0530

    some fixes with map marker icon

--- react-estate-ui/src/components/map/Map.jsx
+++ react-estate-ui/src/components/map/Map.jsx
@@ -1,8 +1,21 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './map.scss'
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import Pin from '../pin/Pin';
 
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 function Map({items}){
   return (
     <MapContainer center={items.length===1?[items[0].latitude,items[0].longitude] :[23.1466, 79.0888]} zoom={5} scrollWheelZoom={false} className='map'>
