145a9b91e5e30a13834a0ab4c8d4a1d3e79e0969
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2024-07-06 02:03:13 +0530

    addded cors for frontend

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ import Messagerouter from "./routes/message.route.js";
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://dreamestate.vercel.app/",
     credentials: true,
 }));
 app.use(express.json());
